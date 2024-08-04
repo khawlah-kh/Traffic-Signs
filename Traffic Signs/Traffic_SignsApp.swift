fec8f0f9af3acb948a4c56a9b3fd639cd0956dd8
Khawlah Khalid <khawlah1920@hotmail.com>
2024-08-04 16:33:25 +0300

    Initial Commit

---
+++ Traffic Signs/Traffic_SignsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Traffic_SignsApp.swift
+//  Traffic Signs
+//
+//  Created by Khawlah Khalid on 04/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Traffic_SignsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
