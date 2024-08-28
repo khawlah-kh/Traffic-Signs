d60f0881818e5fb039b3827f640886609fae68b4
Khawlah Khalid <khawlah1920@hotmail.com>
2024-08-28 09:35:16 +0300

    Read from csv

---
+++ Traffic Signs/ViewModel/TrafficSignManager.swift
@@ -0,0 +1,13 @@
+//
+//  Traffic_Sign_Data.swift
+//  Traffic Signs
+//
+//  Created by Khawlah Khalid on 28/08/2024.
+//
+
+import Foundation
+
+
+class TrafficSignManager{
+    static var idNameDic:[String:String] = [:]
+}
